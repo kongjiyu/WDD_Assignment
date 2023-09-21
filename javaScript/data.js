ad1ff7180f19b7535d10c17f8fa1e0dcb68da7dd
KJiShou <144637328+KJiShou@users.noreply.github.com>
2023-09-21 10:42:39 +0800

    Create data.js

---
+++ javaScript/data.js
@@ -0,0 +1,28 @@
+let menuItemsData = [{
+    id:"prawnmee",
+    name:"PRAWN MEE",
+    price:"10.00",
+    desc:"It primarily consists of rice </br>vermicelli and thicker yellow egg noodles, while the broth is made with prawn heads and shells, </br>and pork ribs.Sliced prawns or shrimps are also added into the dish, along with pork slices, hard </br>boiled eggs, and kangkung .",
+    img:"../image/newPrawnmee.jpg"
+},
+{
+    id:"wantanmee",
+    name:"WANTAN MEE",
+    price:"10.00",
+    desc:"The dish usually consists of egg noodles served in a hot broth, garnished with leafy vegetables and wonton dumplings. The types of leafy vegetables used are usually gai-lan, also known as Chinese broccoli or Chinese kale. Another type of dumpling known as shui jiao (水餃) is sometimes served in place of wonton. ",
+    img:"../image/Newwantanmee.jpg"
+},
+{
+    id:"porknoodle",
+    name:"PORK NOODLE",
+    price:"10.00",
+    desc:"It mainly uses kway teow, fish balls and fish cakes as the main ingredients. Adding the soup made with pork bones gives him a delicious and sweet soup base.",
+    img:"../image/Newporknoodle.jpg"
+},
+{
+    id:"charkwayteow",
+    name:"CHAR KWEY TEOW",
+    price:"10.00",
+    desc:"It is made from flat rice noodles or kway teow of approximately 1 cm or about 0.5 cm in width, stir-fried over very high heat with garlic, light and dark soy sauce, chilli paste, whole prawns, shelled blood cockles, chopped Chinese chives, slices of Chinese sausage, and bean sprouts.",
+    img:"../image/Newcharkwayteow.jpg"
+}]
