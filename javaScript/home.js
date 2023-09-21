86127f59f3dc48319b7e13cc3e82a35b8e331af7
kongjiyu <133716765+kongjiyu@users.noreply.github.com>
2023-09-21 20:19:24 +0800

    slide show
    change slide so that it auto change

--- javaScript/home.js
+++ javaScript/home.js
@@ -1,9 +1,28 @@
-document.querySelectorAll('.s_button')[1].onclick = () => {
+let _direction = true;
+let _move = () => {
     let lists = document.querySelectorAll('.item');
-    document.querySelector('#slide').appendChild(lists[0]); 
+    if(_direction){
+        document.querySelector('#slide').appendChild(lists[0]); 
+    }else{
+        document.querySelector('#slide').prepend(lists[lists.length - 1]); 
+    }
+
+}
+
+let timer = setInterval(_move, 2500);
+document.querySelector('.intro').addEventListener('mouseover',() => {
+    clearInterval(timer);
+})
+document.querySelector('.intro').addEventListener('mouseout',() => {
+    timer = setInterval(_move, 2500);
+})
+
+document.querySelectorAll('.s_button')[1].onclick = () => {
+    _move();
 }
 
 document.querySelectorAll('.s_button')[0].onclick = () => {
-    let lists = document.querySelectorAll('.item');
-    document.querySelector('#slide').prepend(lists[lists.length - 1]); 
+    _direction = false;
+    _move();
 }
+
