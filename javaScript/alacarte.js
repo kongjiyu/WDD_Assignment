65d06ad5210ae5d98874b17395c991cd97633b1a
KJiShou <144637328+KJiShou@users.noreply.github.com>
2023-09-22 13:27:18 +0800

    Update alacarte.js

--- javaScript/alacarte.js
+++ javaScript/alacarte.js
@@ -1,44 +1,17 @@
 let menu = document.getElementById("menu");
-
-let menuItemsData = [{
-    id:"prawnmee",
-    name:"PRAWN MEE",
-    price:"RM10.00",
-    desc:"It primarily consists of rice </br>vermicelli and thicker yellow egg noodles, while the broth is made with prawn heads and shells, </br>and pork ribs.Sliced prawns or shrimps are also added into the dish, along with pork slices, hard </br>boiled eggs, and kangkung .",
-    img:"../image/newPrawnmee.jpg"
-},
-{
-    id:"wantanmee",
-    name:"WANTAN MEE",
-    price:"RM10.00",
-    desc:"The dish usually consists of egg noodles served in a hot broth, garnished with leafy vegetables and wonton dumplings. The types of leafy vegetables used are usually gai-lan, also known as Chinese broccoli or Chinese kale. Another type of dumpling known as shui jiao (水餃) is sometimes served in place of wonton. ",
-    img:"../image/Newwantanmee.jpg"
-},
-{
-    id:"porknoodle",
-    name:"PORK NOODLE",
-    price:"RM10.00",
-    desc:"It mainly uses kway teow, fish balls and fish cakes as the main ingredients. Adding the soup made with pork bones gives him a delicious and sweet soup base.",
-    img:"../image/Newporknoodle.jpg"
-},
-{
-    id:"charkwayteow",
-    name:"CHAR KWEY TEOW",
-    price:"RM10.00",
-    desc:"It is made from flat rice noodles or kway teow of approximately 1 cm or about 0.5 cm in width, stir-fried over very high heat with garlic, light and dark soy sauce, chilli paste, whole prawns, shelled blood cockles, chopped Chinese chives, slices of Chinese sausage, and bean sprouts.",
-    img:"../image/Newcharkwayteow.jpg"
-}]
+let sideDish = document.getElementById("side-dish-menu");
+let drink = document.getElementById("drink-menu");
 
 let cart = JSON.parse(localStorage.getItem("data")) || [];
 
-let generateMenu =() =>{
-    return (menu.innerHTML = menuItemsData
+let generateAlacarteMenu =() =>{
+    return (menu.innerHTML = alacarteItemsData
         .map((x)=>{
             let { id, name, price, desc, img } = x;
             let search = cart.find((x)=> x.id === id) || []
         return `
         <div class="single-menu">
-            <img src=${img} alt="prawnmee">
+            <img src=${img} alt="${id}" class="alacarte-image">
             <div class="menu-content">
                 <h4>${name}<span>RM${price}</span></h4>
                 <p>${desc}</p>
@@ -50,7 +23,49 @@ let generateMenu =() =>{
     }).join(""));
 };
 
-generateMenu();
+generateAlacarteMenu();
+
+let generateSideDishMenu =() =>{
+    return (sideDish.innerHTML = sideDishItemsData
+        .map((x)=>{
+            let { id, name, price, desc, img } = x;
+            let search = cart.find((x)=> x.id === id) || []
+        return `
+        <div class="single-menu">
+            <img src=${img} alt="${id}" class="side-dish-image">
+            <div class="menu-content">
+                <h4>${name}<span>RM${price}</span></h4>
+                <p>${desc}</p>
+                    <button  onclick="increment(${id})" class="addToCart">Add To Cart</button>
+                    <div id=${id}>${search.item === undefined? 0: search.item}</div>
+            </div>
+        </div>
+        `;
+    }).join(""));
+};
+
+generateSideDishMenu();
+
+let generateDrinkshMenu =() =>{
+    return (drink.innerHTML = drinksItemsData
+        .map((x)=>{
+            let { id, name, price, desc, img } = x;
+            let search = cart.find((x)=> x.id === id) || []
+        return `
+        <div class="single-menu">
+            <img src=${img} alt="${id}" class="drink-image">
+            <div class="menu-content">
+                <h4>${name}<span>RM${price}</span></h4>
+                <p>${desc}</p>
+                    <button  onclick="increment(${id})" class="addToCart">Add To Cart</button>
+                    <div id=${id}>${search.item === undefined? 0: search.item}</div>
+            </div>
+        </div>
+        `;
+    }).join(""));
+};
+
+generateDrinkshMenu();
 
 let increment = (id) => {
     let selectedItem = id;
@@ -72,6 +87,7 @@ let increment = (id) => {
 
     update(selectedItem.id);
     localStorage.setItem("data", JSON.stringify(cart));
+    calculation();
 };
 
 let decrement = (id) => {
