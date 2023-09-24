9d443718730125c4ac5683df2e5b6ee7eda62bc8
Kong Ji Yu <kongjiyu0198@gmail.com>
2023-09-25 07:18:04 +0800

    login, home page, animation

--- javaScript/header.js
+++ javaScript/header.js
@@ -1,23 +1,52 @@
-const wrapper = document.querySelector('.wrapper'); 
+const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
+const btnPopup2 = document.querySelector('footer ul li a.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-const input = document.querySelectorAll('.input-box input')
 
-registerLink.addEventListener('click',()=>{
+registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 })
 
-loginLink.addEventListener('click',()=>{
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 })
 
-btnPopup.addEventListener('click',()=>{
+btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
 })
 
-iconClose.addEventListener('click',()=>{
+btnPopup2.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+})
+
+iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 })
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealTop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+var submitButton = document.querySelectorAll(button[type = "submit"]);
+submitButton.addEventListener('click', SubmitForm());
+
+function SubmitForm(event) {
+    event.preventDefault;
+}
 
