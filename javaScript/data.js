a3339427de3bf73951bdeff611843400260e70f8
KJiShou <144637328+KJiShou@users.noreply.github.com>
2023-09-25 20:54:51 +0800

    Update data.js

--- javaScript/data.js
+++ javaScript/data.js
@@ -97,6 +97,105 @@ let menuItemsData = [{
     img:"../image/iced-water.jpg"
 }];
 
+let OrimenuItemsData = [{
+    id:"prawnmee",
+    name:"PRAWN MEE",
+    price:"10.00",
+    desc:"It primarily consists of rice </br>vermicelli and thicker yellow egg noodles, while the broth is made with prawn heads and shells, </br>and pork ribs.Sliced prawns or shrimps are also added into the dish, along with pork slices, hard </br>boiled eggs, and kangkung .",
+    img:"../image/newPrawnmee.jpg"
+},
+{
+    id:"wantanmee",
+    name:"WANTAN MEE",
+    price:"10.00",
+    desc:"The dish usually consists of egg noodles served in a hot broth, garnished with leafy vegetables and wonton dumplings. The types of leafy vegetables used are usually gai-lan, also known as Chinese broccoli or Chinese kale. Another type of dumpling known as shui jiao (水餃) is sometimes served in place of wonton. ",
+    img:"../image/Newwantanmee.jpg"
+},
+{
+    id:"porknoodle",
+    name:"PORK NOODLE",
+    price:"10.00",
+    desc:"It mainly uses kway teow, fish balls and fish cakes as the main ingredients. Adding the soup made with pork bones gives him a delicious and sweet soup base.",
+    img:"../image/Newporknoodle.jpg"
+},
+{
+    id:"charkwayteow",
+    name:"CHAR KWEY TEOW",
+    price:"10.00",
+    desc:"It is made from flat rice noodles or kway teow of approximately 1 cm or about 0.5 cm in width, stir-fried over very high heat with garlic, light and dark soy sauce, chilli paste, whole prawns, shelled blood cockles, chopped Chinese chives, slices of Chinese sausage, and bean sprouts.",
+    img:"../image/Newcharkwayteow.jpg"
+},
+{
+    id:"gyoza",
+    name:"Gyoza",
+    price:"6.00",
+    desc:"Japanese-style bite-sized pockets of deliciousness that filled with a savory mixture of ground meat (such as pork or chicken), cabbage, garlic, and ginger, wrapped in a thin dough. Crispy bottom while keeping the filling tender andjuicy and combination of flavors and textures is simply irresistible. Dip them in a tangy soy-based sauce, and you're in for a taste sensation that will leave you craving more.",
+    img:"../image/gyoza.jpg"
+},
+{
+    id:"chickenpopcorn",
+    name:"Chicken Popcorn",
+    price:"5.00",
+    desc:"American-style bite-sized delight with its crispy exterior and juicy, tender interior that satisfies your cravings.Perfect for parties or as a quick snack, these little nuggets of goodness bring joy to any occasion. So grab a handful and enjoy the irresistible taste of chicken popcorn.</p>",
+    img:"../image/chicken-popcorn.jpg"
+},
+{
+    id:"friedtofu",
+    name:"Fried Tofu",
+    price:"5.00",
+    desc:"A crispy exterior and soft, silky interior and delicious vegetarian option that can be marinated in flavorful sauces or seasoned with aromatic spices.It can be enjoyed on its own as a snack, added to noodles or used as a substitute in various dishes. So, if you're looking for a tasty and satisfying plant-based option, give fried tofu a try.",
+    img:"../image/fried-tofu.jpg"
+},
+{
+    id:"springrolls",
+    name:"Spring Rolls",
+    price:"7.00",
+    desc:"crispy and flavorful delights that filled with a medley of fresh vegetables, fragrant herbs bring a taste of Asia to your plate. Wrapped in a thin, translucent rice paper and then fried or served fresh, dip them in a tangy sauce, spring rolls offer a delightful combination of textures and a burst of vibrant flavors.",
+    img:"../image/spring-rolls.jpg"
+},
+{
+    id:"tempura",
+    name:"Tempura",
+    price:"6.00",
+    desc:"A crispy and delightful Japanese dish that coats various ingredients like shrimp, vegetables, or even tofu, creating a satisfying crunch. Dip each piece into a special sauce and experience a harmonious blend of umami and crispiness. whether enjoyed as an appetizer or part of a meal.",
+    img:"../image/tempura.jpg"
+},
+{
+    id:"lemontea",
+    name:"Iced Lemon Tea",
+    price:"3.00",
+    desc:"A refreshing and tangy beverage that provides a burst of revitalizing flavors with its combination of zesty lemon and cool tea. Served over ice, the balance of sweetness and tartness that is truly perfection whether is enjoyed on a sunny day or as a refreshing accompaniment to a meal.",
+    img:"../image/iced-lemon-tea.jpg"
+},
+{
+    id:"coffee",
+    name:"Iced Coffee",
+    price:"3.50",
+    desc:"A cool and invigorating beverage that gives you a refreshing jolt of energy with its rich and bold flavor. Served over ice and a splash of milk or flavored syrups, it offers a delightful combination of smoothness and a hint of bitterness.",
+    img:"../image/iced-coffee.jpg"
+},
+{
+    id:"cincau",
+    name:"Soya Bean Cincau",
+    price:"3.00",
+    desc:"A refreshing and unique Asian beverage. Served chilled with its combination of creamy soya milk and jelly-like cincau (grass jelly), this drink offers a delightful contrast of textures. Whether sipped as a refreshing treat on a hot day or as a dessert after a spicy meal, soya bean cincau is a beloved beverage that brings a taste of Indonesia to your palate.",
+    img:"../image/soya-bean-cincau.jpg"
+},
+{
+    id:"coke",
+    name:"Coke",
+    price:"2.50",
+    desc:"An iconic carbonated beverage that brings a fizzy burst of sweetness. Sip it chilled whether enjoyed on its own or mixed with a splash of your favorite spirit, coke is a classic choice that never fails to quench your thirst.",
+    img:"../image/coke.jpg"
+},
+{
+    id:"water",
+    name:"Iced Water",
+    price:"1.50",
+    desc:"A common refresher. With its crisp and refreshing taste, it provides instant relief on a hot day. Served ice-cold, it revitalizes your senses and hydrates your body. Stay cool and hydrated with a simple glass of iced water.",
+    img:"../image/iced-water.jpg"
+}];
+
 let alacarteItemsData = [{
     id:"prawnmee",
     name:"PRAWN MEE",
@@ -162,12 +261,12 @@ let sideDishItemsData = [{
     img:"../image/tempura.jpg"
 }];
 
-let drinkstemsData = [{
-    id:"leontea",
+let drinksItemsData = [{
+    id:"lemontea",
     name:"Iced Lemon Tea",
     price:"3.00",
     desc:" refreshing and tangy beverage that provides a burst of revitalizing flavors with its combination of zesty lemon and cool tea. Served over ice, the balance of sweetness and tartness that is truly perfection whether is enjoyed on a sunny day or as a refreshing accompaniment to a meal.",
-    img:"./image/iced-lemon-tea.jpg"
+    img:"../image/iced-lemon-tea.jpg"
 },
 {
     id:"cofee",
@@ -197,4 +296,3 @@ let drinkstemsData = [{
     desc:"A common refresher. With its crisp and refreshing taste, it provides instant relief on a hot day. Served ice-cold, it revitalizes your senses and hydrates your body. Stay cool and hydrated with a simple glass of iced water.",
     img:"../image/iced-water.jpg"
 }]
-
