1100b3e9630067f387c0adfa2465acd64241fe14
KJiShou <144637328+KJiShou@users.noreply.github.com>
2023-09-25 20:55:05 +0800

    Update drinks.js

--- javaScript/drinks.js
+++ javaScript/drinks.js
@@ -1,55 +1,74 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+let drink = document.getElementById("drink-menu");
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
+let cart = JSON.parse(localStorage.getItem("data")) || [];
 
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
+let generateDrinkshMenu =() =>{
+    return (drink.innerHTML = drinksItemsData
+        .map((x)=>{
+            let { id, name, price, desc, img } = x;
+            let search = cart.find((x)=> x.id === id) || []
+        return `
+        <div class="single-menu">
+            <img src=${img} alt="${id}" class="drink-image">
+            <div class="menu-content">
+                <h4>${name}<span>RM${price}</span></h4>
+                <p>${desc}</p>
+                    <button class="buttonmain" onclick="increment(${id})" class="addToCart">Add To Cart</button>
+            </div>
+        </div>
+        `;
+    }).join(""));
+};
 
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
+generateDrinkshMenu();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = cart.find((x)=> x.id=== selectedItem.id);
+
+    if(search === undefined) {
+        cart.push({
+            id: selectedItem.id,
+            item: 1,
+            
+        }
+        );
+    }
+    else{
+        search.item +=1;
+    };
+    
+    alert("Added to the cart! ");
+
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(cart));
+    calculation();
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = cart.find((x)=> x.id=== selectedItem.id);
+
+    if(search===undefined) return;
+    if(search === 0) return;
+    else{
+        search.item -=1;
+    };
+    
+    update(selectedItem.id);
+    cart = cart.filter ((x) => x.item !==0);
+    localStorage.setItem("data", JSON.stringify(cart));
 }
+let update = (id) => {
+    let search = cart.find((x) => x.id === id);
+    console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+};
 
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
+let calculation =()=> {
+    let cartIcon = document.getElementById("total");
+    cartIcon.innerHTML = cart.map((x) => x.item).reduce((x,y) => x+y, 0);
+};
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal2 = document.querySelector(button.dataset.modalTarget)
-        openModal(modal2)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal2 = button.closest('.modal')
-        closeModal(modal2)
-    })
-})
-
-function openModal(modal2) {
-    if (modal2 == null) return
-    modal2.classList.add('active')
-    overlay.classList.add('active')
-}
-
-function closeModal(modal2) {
-    if (modal2 == null) return
-    modal2.classList.remove('active')
-    overlay.classList.remove('active')
-}
+calculation();
